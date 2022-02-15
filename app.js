c752f24dac0a79a5b0018226ce0514897612b5d2
Ishaan Kalia <sticktrick3181@gmail.com>
2022-02-15 09:10:07 +0530

    Port number

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", path.join(__dirname, "views"));
 //SERVING STATIC FILES
 app.use(express.static(path.join(__dirname, "public")));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const morgan = require("morgan");
 const tripRouter = require("./routes/tripRoutes");
